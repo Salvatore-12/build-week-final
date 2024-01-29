70453bedf479f839ccd3915a787f4e5ac724f759
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-01-29 11:46:28 +0100

    first commit

---
+++ src/main/java/Team3/buildweekfinal/BuildWeekFinalApplication.java
@@ -0,0 +1,13 @@
+package Team3.buildweekfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildWeekFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildWeekFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Team3/buildweekfinal/BuildWeekFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package Team3.buildweekfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildWeekFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
