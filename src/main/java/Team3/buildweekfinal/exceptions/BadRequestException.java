582f2ca6a23d77583c8ceaedc07f8d7396debb98
OleksandrLarionov <larionov.alex23@gmail.com>
2024-01-29 18:22:28 +0100

    handler update

--- src/main/java/Team3/buildweekfinal/exceptions/BadRequestException.java
+++ src/main/java/Team3/buildweekfinal/exceptions/BadRequestException.java
@@ -1,8 +1,18 @@
 package Team3.buildweekfinal.exceptions;
 
+import lombok.Getter;
+import org.springframework.validation.ObjectError;
+
+import java.util.List;
+@Getter
 public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message){
+    private List<ObjectError> errorList;
+    public BadRequestException(String message) {
         super(message);
+    }
+    public BadRequestException(List<ObjectError> errorList) {
+        super("Errore nel payload");
+        this.errorList = errorList;
     }
 }
 
--- src/main/java/Team3/buildweekfinal/exceptions/ExceptionsHandler.java
+++ src/main/java/Team3/buildweekfinal/exceptions/ExceptionsHandler.java
@@ -1,29 +1,60 @@
 package Team3.buildweekfinal.exceptions;
 
+import Team3.buildweekfinal.payloads.ErrorsDTO;
+import Team3.buildweekfinal.payloads.ErrorsPayloadWhitList;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RestControllerAdvice
 public class ExceptionsHandler {
-    @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorsPayload handleNotFound(NotFoundException ex){
-        return new ErrorsPayload(ex.getMessage(), LocalDateTime.now());
-    }
-
     @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorsPayload handleBadRequest(BadRequestException ex){
-        return new ErrorsPayload(ex.getMessage(),LocalDateTime.now());
+    public ErrorsPayloadWhitList handleBadRequest(BadRequestException ex) {
+        List<String> errorsMessages = new ArrayList<>();
+        if(ex.getErrorList() != null)
+            errorsMessages = ex.getErrorList().stream().map(errore -> errore.getDefaultMessage()).toList();
+        return new ErrorsPayloadWhitList(ex.getMessage(), newDateAndHour(), errorsMessages);
     }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED) // 401
+    public ErrorsDTO handleUnauthorized(UnauthorizedException e) {
+        return new ErrorsDTO(e.getMessage(),newDateAndHour());
+    }
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorsDTO accessDenied(AccessDeniedException ex){
+        return new ErrorsDTO("Access Denied", newDateAndHour());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorsDTO handleNotFound(NotFoundException ex) {
+        return new ErrorsDTO(ex.getMessage(),newDateAndHour());
+    }
+
+
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorsPayload handleGenericException(Exception ex){
+    public ErrorsDTO handleGenericError(Exception ex) {
         ex.printStackTrace();
-        return new ErrorsPayload("errore lato server,risolveremo il problema al più presto",LocalDateTime.now());
+        return new ErrorsDTO("Work in progress!", newDateAndHour());
+    }
+
+    public static String newDateAndHour(){
+        String pattern = "E, dd MMM yyyy HH:mm:ss";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        String date = simpleDateFormat.format(new Date());
+        return date;
     }
 }
