c7c0365cc7dd528c5bc1a01e23100f20c3921ddd
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-01-29 17:35:34 +0100

    Add Cartella Exceptions con le varie classi degli errori

---
+++ src/main/java/Team3/buildweekfinal/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package Team3.buildweekfinal.exceptions;
+
+public class UnauthorizedException extends RuntimeException{
+    public UnauthorizedException(String message){
+        super(message);
+    }
+}
