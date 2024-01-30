0118ad6202bfc50aa12641d85c7c17ed82fcf899
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-01-30 11:04:24 +0100

    ADDED CLOUDINARY NEL USERS CONTROLLERS

--- src/main/java/Team3/buildweekfinal/services/UsersService.java
+++ src/main/java/Team3/buildweekfinal/services/UsersService.java
@@ -3,13 +3,17 @@ import Team3.buildweekfinal.payloads.UsersDTO;
 import Team3.buildweekfinal.entities.User;
 import Team3.buildweekfinal.exceptions.NotFoundException;
 import Team3.buildweekfinal.repositories.UsersDAO;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -19,6 +23,8 @@ public class UsersService
 {
     @Autowired
     private UsersDAO usersDAO;
+    @Autowired
+    private Cloudinary cloudinaryUploader;
 
     public static Optional<User> getUserById(UUID idUser) {
         return null;
@@ -62,6 +68,14 @@ public class UsersService
         found.setPassword(body.password());//inserire bcrypt
         return usersDAO.save(found);
     }
+    public String uploadPicture(MultipartFile file) throws IOException {
+
+        String url = (String) cloudinaryUploader.uploader()
+                .upload(file.getBytes(), ObjectUtils.emptyMap())
+                .get("url");
+        return url;
+    }
+
     public User findByEmail(String email)
     {
         return usersDAO.findByEmail(email).orElseThrow(()->new NotFoundException(email));
