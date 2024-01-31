17cbb2588c0a4b7aeaad81982a1c914dc1479676
OleksandrLarionov <larionov.alex23@gmail.com>
2024-01-31 12:01:34 +0100

    Update UserController.java

--- src/main/java/Team3/buildweekfinal/controllers/UserController.java
+++ src/main/java/Team3/buildweekfinal/controllers/UserController.java
@@ -1,6 +1,5 @@
 package Team3.buildweekfinal.controllers;
 
-import Team3.buildweekfinal.entities.Bill;
 import Team3.buildweekfinal.entities.User;
 import Team3.buildweekfinal.exceptions.BadRequestException;
 import Team3.buildweekfinal.payloads.UpdateExistingUserDTO;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.UUID;
 
 
